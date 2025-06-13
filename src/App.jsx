a437721f67eeae8fcaf61ceaf771c730403df93d
Daniel Procop <danielprocop13@gmail.com>
2025-06-14 00:19:35 +0200

    Co-authored-by: Andy Simota <andyiulian.simota@gmail.com> Co-authored-by: adrianprocop92 <adrianprocop92@users.noreply.github.com>add auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AddUser from "./pages/AddUser";
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
-function App() {
+function App({ signOut, user }) {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/skills" element={<AddUser />} />
-      </Routes>
-    </Router>
+    <>
+      <header style={{ padding: "1rem", background: "#f3f3f3" }}>
+        <span>👤 {user.username}</span>
+        <button onClick={signOut} style={{ marginLeft: "1rem" }}>
+          Logout
+        </button>
+      </header>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<AddUser />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
