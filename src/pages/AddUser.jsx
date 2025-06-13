f63e76d620e58ca0af25b637046bb36dd8da64f8
Andy Simota <andyiulian.simota@gmail.com>
2025-06-13 23:32:19 +0200

    changed path

--- src/pages/AddUser.jsx
+++ src/pages/AddUser.jsx
@@ -1,11 +1,11 @@
-import AddUserForm from "../components/AddUserForm";
+import AddSkillsForm from "../components/AddSkillsForm";
 
 export default function AddUser() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="max-w-md w-full p-6 bg-white rounded-xl shadow-md">
         <h2 className="text-2xl font-bold mb-4 text-center">Aggiungi utente</h2>
-        <AddUserForm />
+        <AddSkillsForm />
       </div>
     </div>
   );
